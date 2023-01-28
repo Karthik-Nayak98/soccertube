47ebf2e7545120f30ee0493efe98b643b277af9b
karthik nayak <nayakkarthik466@gmail.com>
2023-01-28 20:32:46 +0530

    add explore page to render videos

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,30 @@
+import { Container, Flex, SimpleGrid, useBreakpointValue } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { VideoCard } from '../components';
+
+const smVariant = { navigation: 'drawer' };
+const mdVariant = { navigation: 'sidebar' };
+
+function Explore() {
+  const [videos, setVideos] = useState();
+  const variants = useBreakpointValue({ base: smVariant, md: mdVariant });
+
+  useEffect(() => {
+    axios.get('/api/videos').then((response) => setVideos(response.data.videos));
+  }, []);
+
+  return (
+    <Flex height='100vh'>
+      <Container maxW='5xl'>
+        <SimpleGrid columns={[1, 2, 3]} spacing='1.5rem' mt={4}>
+          {videos?.map((video) => (
+            <VideoCard key={video._id} {...video} />
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Flex>
+  );
+}
+
+export default Explore;
