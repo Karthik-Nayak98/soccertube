4865f2e412be35c7cca43bc13f5a9d8edf9b3a1d
karthik nayak <nayakkarthik466@gmail.com>
2023-01-28 20:36:55 +0530

    add badges to display different categories

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,11 @@
-import { Container, Flex, SimpleGrid, useBreakpointValue } from '@chakra-ui/react';
+import {
+  Badge,
+  Container,
+  Flex,
+  SimpleGrid,
+  useBreakpointValue,
+  Wrap,
+} from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { VideoCard } from '../components';
@@ -19,6 +26,48 @@ function Explore() {
     <Flex height='100vh'>
       <SideBar variant={variants?.navigation} />
       <Container maxW='5xl'>
+        <Wrap mt={4}>
+          <Badge
+            variant='outline'
+            px={[2, 3, 4]}
+            py={[0.6, 0.8, 1]}
+            fontSize={['0.85rem', '0.95rem', '1rem']}
+            borderRadius={8}>
+            All
+          </Badge>
+          <Badge
+            variant='outline'
+            px={[2, 3, 4]}
+            py={[0.6, 0.8, 1]}
+            fontSize={['0.85rem', '0.95rem', '1rem']}
+            borderRadius={8}>
+            Best Goals
+          </Badge>
+          <Badge
+            variant='outline'
+            px={[2, 3, 4]}
+            py={[0.6, 0.8, 1]}
+            fontSize={['0.85rem', '0.95rem', '1rem']}
+            borderRadius={8}>
+            Best Save
+          </Badge>
+          <Badge
+            variant='outline'
+            px={[2, 3, 4]}
+            py={[0.6, 0.8, 1]}
+            fontSize={['0.85rem', '0.95rem', '1rem']}
+            borderRadius={8}>
+            Skills
+          </Badge>
+          <Badge
+            variant='outline'
+            px={[2, 3, 4]}
+            py={[0.6, 0.8, 1]}
+            fontSize={['0.85rem', '0.95rem', '1rem']}
+            borderRadius={8}>
+            Best Freekick
+          </Badge>
+        </Wrap>
         <SimpleGrid columns={[1, 2, 3]} spacing='1.5rem' mt={4}>
           {videos?.map((video) => (
             <VideoCard key={video._id} {...video} />
