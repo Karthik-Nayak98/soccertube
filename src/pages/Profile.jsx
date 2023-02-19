a48447a0dc7e697425bdbb805943c215bdfa097e
karthik nayak <nayakkarthik466@gmail.com>
2023-02-19 18:07:23 +0530

    add user profile page

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,61 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { SideBar } from '../layouts/';
+
+const smVariant = { navigation: 'drawer' };
+const mdVariant = { navigation: 'sidebar' };
+
+function Profile() {
+  const variants = useBreakpointValue({ base: smVariant, md: mdVariant });
+
+  const { user, signOut } = useAuth();
+
+  function handleSignout() {
+    signOut();
+  }
+
+  return (
+    <Flex>
+      <SideBar variant={variants?.navigation} />
+      <Container maxW={['100%', '75%']} my={4} mr='2rem'>
+        <Center h='75vh'>
+          <Card w='300px'>
+            <Heading px='4' pt='3' size='md'>
+              Your Profile
+            </Heading>
+            <CardBody>
+              <Text>
+                <Text as='span' fontWeight={600}>
+                  Name:{' '}
+                </Text>
+                {user?.user_metadata.name}
+              </Text>
+              <Text>
+                <Text as='span' fontWeight={600}>
+                  Email:{' '}
+                </Text>
+                {user?.email}
+              </Text>
+              <Button size='md' mt='4' colorScheme='blue' onClick={handleSignout}>
+                Signout
+              </Button>
+            </CardBody>
+          </Card>
+        </Center>
+      </Container>
+    </Flex>
+  );
+}
+
+export default Profile;
