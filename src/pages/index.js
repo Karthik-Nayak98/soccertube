2c99fb7484579fdd0cfb182dd6f308d19d91227d
karthik nayak <nayakkarthik466@gmail.com>
2023-02-19 18:08:36 +0530

    index page to export all the pages

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Error from './Error';
+import Explore from './Explore';
+import Home from './Home';
+import Liked from './Liked';
+import Login from './Login';
+import Playlist from './Playlist';
+import Profile from './Profile';
+import Register from './Register';
+import Video from './Video';
+import WatchLater from './WatchLater';
+
+export {
+  Error,
+  Profile,
+  Playlist,
+  WatchLater,
+  Liked,
+  Explore,
+  Video,
+  Register,
+  Login,
+  Home,
+};
