6cc3e4b36b977e43a24c59e77dccc2e6ae09b83b
karthik nayak <nayakkarthik466@gmail.com>
2023-03-20 21:34:16 +0530

    add playlistCard component to index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import BadgeList from './BadgeList';
+import KebabMenu from './KebabMenu';
+import Menu from './Menu';
+import MenuItem from './MenuItem';
+import PlaylistCard from './PlaylistCard';
+import VideoCard from './VideoCard';
+
+export { MenuItem, Menu, VideoCard, PlaylistCard, BadgeList, KebabMenu };
