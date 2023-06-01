ec0ff46ae86f7e67424fb7f1ddfd6071eb7094df
karthik nayak <nayakkarthik466@gmail.com>
2023-06-01 19:44:38 +0530

    Add configuration field to get data from supabase.
    * Remove SimpleGrid component.

--- src/hoc/withVideos.jsx
+++ src/hoc/withVideos.jsx
@@ -1,8 +1,8 @@
-import { Container, Flex, SimpleGrid, useBreakpointValue } from '@chakra-ui/react';
+import { Container, Flex, useBreakpointValue } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useErrorBoundary } from 'react-error-boundary';
-import { EmptyVideoContainer } from '../components';
+import { EmptyVideoContainer, SpinnerContainer } from '../components';
 import useFetch from '../hooks/useFetch';
 import { SideBar } from '../layouts';
 import supabase from '../supabase/client';
@@ -13,24 +13,36 @@ const mdVariant = { navigation: 'sidebar' };
 const withVideos = (configuration) => (Component) => () => {
   const variants = useBreakpointValue({ base: smVariant, md: mdVariant });
   const [videos, setVideos] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
   const [loading, setLoading] = useState(false);
   const { showBoundary } = useErrorBoundary();
 
   const { data, error, isLoading } = useFetch(configuration.table);
 
-  const handleDelete = async (id) => {
+  const handleDelete = async (data_id) => {
     try {
       setLoading(true);
-      await supabase.from(configuration.table).delete().eq('video_id', id);
-      setVideos((prevVideos) => prevVideos.filter(({ _id }) => id != _id));
+      await supabase
+        .from(configuration.table)
+        .delete()
+        .eq(configuration.field, data_id);
+
+      if (configuration.table === 'playlist') {
+        setPlaylist((prevPlaylist) =>
+          prevPlaylist.filter(({ id }) => data_id != id)
+        );
+      } else {
+        setVideos((prevVideos) => prevVideos.filter(({ _id }) => data_id != _id));
+      }
     } catch (error) {
       showBoundary(error);
     } finally {
       setLoading(false);
     }
   };
+
   useEffect(() => {
-    async function getLikedVideos() {
+    async function getVideos() {
       const response = await axios.get('/api/videos');
 
       const videos = data?.map(({ video_id }) =>
@@ -40,7 +52,14 @@ const withVideos = (configuration) => (Component) => () => {
       );
       setVideos(videos);
     }
-    getLikedVideos();
+    getVideos();
+  }, [data]);
+
+  useEffect(() => {
+    function getPlaylist() {
+      setPlaylist(data || []);
+    }
+    getPlaylist();
   }, [data]);
 
   if (isLoading || loading) return <SpinnerContainer />;
@@ -52,13 +71,11 @@ const withVideos = (configuration) => (Component) => () => {
     <Flex>
       <SideBar variant={variants?.navigation} />
       <Container maxW='5xl' mr='2rem'>
-        <SimpleGrid columns={[1, 2, 3]} spacing='1.5rem' mt={4}>
-          <Component
-            supabaseData={data}
-            videos={videos}
-            handleDelete={handleDelete}
-          />
-        </SimpleGrid>
+        <Component
+          supabaseData={Array.isArray(playlist) ? playlist : []}
+          videos={videos}
+          handleDelete={handleDelete}
+        />
       </Container>
     </Flex>
   );
