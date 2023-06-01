5689cfcca2bbc1bda79c5c75d8b1a892770a570b
karthik nayak <nayakkarthik466@gmail.com>
2023-06-01 20:00:47 +0530

    Add state to the link component to route.

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,10 +3,10 @@ import { PropTypes } from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function MenuItem({ url, title, icon }) {
+function MenuItem({ url, title, icon, route }) {
   return (
-    <Link _active={{ bg: 'gray.200' }} to={url}>
-      <ListItem _hover={{ bg: 'gray.100' }} py={1}>
+    <Link _active={{ bg: 'gray.200' }} to={url} state={{ route: route }}>
+      <ListItem _hover={{ bg: 'gray.100' }} py={1} px={2}>
         <ListIcon as={icon} />
         {title}
       </ListItem>
@@ -16,8 +16,9 @@ function MenuItem({ url, title, icon }) {
 
 MenuItem.propTypes = {
   url: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.func.isRequired,
 };
 
 export default MenuItem;
